cdc3a15446df2bbe2dbfce926783b14ec25f2464
a7mad1112 <ahmalawneh79@gmail.com>
2024-01-14 19:21:11 +0200

    encode and decode msg

--- js/script.js
+++ js/script.js
@@ -2,12 +2,8 @@ import HuffmanTree from './huffmanTree.js';
 const sendBtn = document.getElementById('send-btn');
 const textarea = document.getElementById('originalMessage');
 const fileInput = document.getElementById('fileInput');
-let msgToSend = '';
-
-textarea.onchange = (e) => {
-  msgToSend = e.target.value;
-};
-
+const encodedMsg = document.querySelector('#encodedMsg > span');
+const decodedMsg = document.querySelector('#decodedMsg > span');
 fileInput.addEventListener('change', function selectedFileChanged() {
   if (this.files.length === 0) {
     console.error('No file selected.');
@@ -16,12 +12,38 @@ fileInput.addEventListener('change', function selectedFileChanged() {
 
   const reader = new FileReader();
   reader.onload = function fileReadCompleted() {
-    msgToSend = textarea.value = reader.result;
+    textarea.value = reader.result;
   };
   reader.readAsText(this.files[0]);
 });
 
 sendBtn.onclick = (e) => {
   e.preventDefault();
-  console.log(msgToSend);
+  if (textarea.value == '') {
+    alert('Please write a msg...');
+    return;
+  }
+  const huffmanTree = new HuffmanTree();
+  const frequencies = convertTextIntoFrequencies(textarea.value);
+  huffmanTree.buildTree(frequencies);
+  encodedMsg.innerHTML = huffmanTree.encode(textarea.value);
+  decodedMsg.innerHTML = huffmanTree.decode(encodedMsg.innerHTML);
+  textarea.value = '';
+  fileInput.value = null;
+};
+
+const convertTextIntoFrequencies = (txt) => {
+  /*
+  Frequencies Example:
+  {
+  'A': 2,
+  'B': 5,
+  'C': 1,
+  }
+  */
+  const frequencies = {};
+  for (const char of txt) {
+    frequencies[char] = (frequencies[char] || 0) + 1;
+  }
+  return frequencies;
 };
